abd796113db8a507520b3fee6de2bb304d1881eb
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-07-04 14:04:12 +0300

    personelin create i yapıldı

---
+++ CoreMvcPersonel/Controllers/PersonelController.cs
@@ -0,0 +1,28 @@
+﻿using CoreMvcPersonel.Models.Contexts;
+using CoreMvcPersonel.Models.DTOs.PersonelDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMvcPersonel.Controllers
+{
+    public class PersonelController : Controller
+    {
+        private readonly SqlContext sqlContext;
+
+        public PersonelController(SqlContext sqlContext)
+        {
+            this.sqlContext = sqlContext;
+        }
+        public IActionResult Index()
+        {
+            var sonuc = sqlContext.Personeller.Include(p => p.Adresler).Include(p =>
+            p.Fotograflar).ToList();
+            return View(sonuc);
+        }
+        public IActionResult Create()
+        {
+            var personel = new PersonelCreateDTO();
+            return View(personel);
+        }
+    }
+}
---
+++ CoreMvcPersonel/Models/DTOs/PersonelDTOs/PersonelCreateDTO.cs
@@ -0,0 +1,18 @@
+﻿using CoreMvcPersonel.Models.Entites;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreMvcPersonel.Models.DTOs.PersonelDTOs
+{
+    public class PersonelCreateDTO
+    {
+        [Required(ErrorMessage = "Ad alanı zorunludur")]
+        public string Ad { get; set; }
+        [Required(ErrorMessage = "Soyad alanı zorunludur")]
+        public string Soyad { get; set; }
+        public string TcNo { get; set; }
+        public string? Email { get; set; }
+        public string? Gsm { get; set; }
+        public Adres? Adres { get; set; }
+        public Fotograf? Fotograf { get; set; }
+    }
+}
