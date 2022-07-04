4d81d0258f452e1cb571f00e5a4e06c2db543534
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-07-04 10:52:36 +0300

    migrations yapıldı

---
+++ CoreMvcPersonel/Migrations/20220704074435_initDb.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreMvcPersonel.Migrations
+{
+    public partial class initDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personeller",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TcNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gsm = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personeller", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adresler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdresTip = table.Column<int>(type: "int", nullable: true),
+                    SehirId = table.Column<int>(type: "int", nullable: true),
+                    IlceId = table.Column<int>(type: "int", nullable: true),
+                    CaddeSokak = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adresler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adresler_Personeller_PersonelId",
+                        column: x => x.PersonelId,
+                        principalTable: "Personeller",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fotograflar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Foto = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PersonelId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fotograflar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fotograflar_Personeller_PersonelId",
+                        column: x => x.PersonelId,
+                        principalTable: "Personeller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adresler_PersonelId",
+                table: "Adresler",
+                column: "PersonelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fotograflar_PersonelId",
+                table: "Fotograflar",
+                column: "PersonelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adresler");
+
+            migrationBuilder.DropTable(
+                name: "Fotograflar");
+
+            migrationBuilder.DropTable(
+                name: "Personeller");
+        }
+    }
+}
